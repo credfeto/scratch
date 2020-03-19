d6e92559e12347b12a881a3b756636e9ece47c8a
Mark Ridgwell <credfeto@users.noreply.github.com>
2020-03-19 22:48:09 +0000

    Initial Schema

---
+++ testSql/Tables/sales.order_items.sql
@@ -0,0 +1,16 @@
+CREATE TABLE [sales].[order_items]
+(
+[order_id] [int] NOT NULL,
+[item_id] [int] NOT NULL,
+[product_id] [int] NOT NULL,
+[quantity] [int] NOT NULL,
+[list_price] [decimal] (10, 2) NOT NULL,
+[discount] [decimal] (4, 2) NOT NULL CONSTRAINT [DF__order_ite__disco__398D8EEE] DEFAULT ((0))
+) ON [PRIMARY]
+GO
+ALTER TABLE [sales].[order_items] ADD CONSTRAINT [PK__order_it__837942D476E9503E] PRIMARY KEY CLUSTERED  ([order_id], [item_id]) ON [PRIMARY]
+GO
+ALTER TABLE [sales].[order_items] ADD CONSTRAINT [FK__order_ite__produ__3B75D760] FOREIGN KEY ([product_id]) REFERENCES [production].[products] ([product_id]) ON DELETE CASCADE ON UPDATE CASCADE
+GO
+ALTER TABLE [sales].[order_items] ADD CONSTRAINT [FK__order_ite__order__3A81B327] FOREIGN KEY ([order_id]) REFERENCES [sales].[orders] ([order_id]) ON DELETE CASCADE ON UPDATE CASCADE
+GO
---
+++ testSql/Tables/sales.orders.sql
@@ -0,0 +1,20 @@
+CREATE TABLE [sales].[orders]
+(
+[order_id] [int] NOT NULL IDENTITY(1, 1),
+[customer_id] [int] NULL,
+[order_status] [tinyint] NOT NULL,
+[order_date] [date] NOT NULL,
+[required_date] [date] NOT NULL,
+[shipped_date] [date] NULL,
+[store_id] [int] NOT NULL,
+[staff_id] [int] NOT NULL
+) ON [PRIMARY]
+GO
+ALTER TABLE [sales].[orders] ADD CONSTRAINT [PK__orders__465962299C09D4CB] PRIMARY KEY CLUSTERED  ([order_id]) ON [PRIMARY]
+GO
+ALTER TABLE [sales].[orders] ADD CONSTRAINT [FK__orders__customer__34C8D9D1] FOREIGN KEY ([customer_id]) REFERENCES [sales].[customers] ([customer_id]) ON DELETE CASCADE ON UPDATE CASCADE
+GO
+ALTER TABLE [sales].[orders] ADD CONSTRAINT [FK__orders__staff_id__36B12243] FOREIGN KEY ([staff_id]) REFERENCES [sales].[staffs] ([staff_id])
+GO
+ALTER TABLE [sales].[orders] ADD CONSTRAINT [FK__orders__store_id__35BCFE0A] FOREIGN KEY ([store_id]) REFERENCES [sales].[stores] ([store_id]) ON DELETE CASCADE ON UPDATE CASCADE
+GO
