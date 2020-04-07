1119a33f932c53d1a4dcf1e95576239174a7a6e8
Mark Ridgwell <credfeto@users.noreply.github.com>
2020-04-07 22:41:53 +0100

    Split method into two

--- src/Experiments/ReleaseNoteFormatting.cs
+++ src/Experiments/ReleaseNoteFormatting.cs
@@ -1,4 +1,6 @@
 using System;
+using System.Text;
+using System.Text.RegularExpressions;
 using Xunit;
 using Xunit.Abstractions;
 
@@ -6,25 +8,53 @@ namespace Experiments
 {
     public class ReleaseNoteFormatting
     {
-        private readonly ITestOutputHelper _output;
-
-        public ReleaseNoteFormatting( ITestOutputHelper output)
+        public ReleaseNoteFormatting(ITestOutputHelper output)
         {
             _output = output;
         }
-        
-        const string SIMPLE = @"### Added
+
+        private readonly ITestOutputHelper _output;
+
+        private const string SIMPLE = @"### Added
 - Some Stuff
 - Some Other Stuff
 ### Changed
 - FF-1324 - some text
 ";
-            
-        
+
+        private static string Bold(string value)
+        {
+            return "**" + value + "**";
+        }
+
+        private static string Underline(string value)
+        {
+            return "__" + value + "__";
+        }
+
+
         [Fact]
         public void Convert()
         {
             _output.WriteLine(SIMPLE);
+
+            _output.WriteLine("****************************************************");
+            var builder = new StringBuilder();
+            var text = SIMPLE.Split(Environment.NewLine);
+            foreach (var line in text)
+            {
+                if (line.StartsWith("### "))
+                {
+                    var replacement = Bold(Underline(line.Substring(4).Trim()));
+                    builder.AppendLine(replacement);
+                    continue;
+                }
+
+                builder.AppendLine(Regex.Replace(line, "(ff\\-\\d+)", "_$1:_", RegexOptions.IgnoreCase).Trim());
+            }
+
+
+            _output.WriteLine(builder.ToString());
         }
     }
 }
