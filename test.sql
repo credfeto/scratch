7fb9cd9116bae0a093113f14f887b67ea8f6f7f9
Mark Ridgwell <credfeto@users.noreply.github.com>
2020-05-10 14:05:40 +0100

    Changed test.sql to have sql in it

--- test.sql
+++ test.sql
@@ -2,5 +2,7 @@ CREATE TABLE Test.Test (
     Id INT IDENTITY(1, 1) NOT NULL,
     Name VARCHAR(66) NOT NULL,
     CONSTRAINT PK_Test PRIMARY KEY (Id),
-    CONSTRAINT IXU_Name UNIQUE (name)
+    CONSTRAINT IXU_Name UNIQUE (Name)
     )
+
+INSERT INTO Test.Test ( Name ) VALUE ('Hello World')
