1690619c8df87ab414aa32360cd38054f8c5e7ed
Mark Ridgwell <credfeto@users.noreply.github.com>
2020-10-08 19:21:30 +0100

    Compatibility check

---
+++ src/Experiments/MakeMatrix.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Experiments
+{
+    public sealed class MakeMatrix
+    {
+        private readonly ITestOutputHelper _output;
+
+        public MakeMatrix(ITestOutputHelper output)
+        {
+            this._output = output ?? throw new ArgumentNullException(nameof(output));
+        }
+
+        private static CompatibilityCheck<T>[,] Matrix<T>(IReadOnlyList<T> make)
+        {
+            CompatibilityCheck<T>[,] x = new CompatibilityCheck<T>[make.Count, make.Count - 1];
+
+            for (int i = 0; i < make.Count; ++i)
+            {
+                int ypos = 0;
+
+                for (int j = 0; j < make.Count; ++j)
+                {
+                    if (i == j)
+                    {
+                        continue;
+                    }
+
+                    x[i, ypos] = new CompatibilityCheck<T>(make[i], make[j]);
+                    ++ypos;
+                }
+            }
+
+            return x;
+        }
+
+        [Fact]
+        public void Produce()
+        {
+            int[] source = {1, 2, 3, 4};
+
+            (int s, int t)[,] m = Matrix(source);
+
+            foreach ((int s, int t) x in m)
+            {
+                this._output.WriteLine($"{x.s},{x.t}");
+            }
+        }
+
+        private sealed class CompatibilityCheck<T>
+        {
+            public CompatibilityCheck(T from, T to)
+            {
+                this.From = from;
+                this.To = to;
+            }
+
+            public T From { get; }
+
+            public T To { get; }
+
+            public bool? Compatibile { get; set; }
+        }
+    }
+}
