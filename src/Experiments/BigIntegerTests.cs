bcb8c8eb666024b6ff0a96abd7cb625d1a0de83b
Mark Ridgwell <credfeto@users.noreply.github.com>
2021-04-15 12:46:20 +0100

    timespan formatting

--- src/Experiments/BigIntegerTests.cs
+++ src/Experiments/BigIntegerTests.cs
@@ -25,4 +25,26 @@ namespace Experiments
             this._output.WriteLine($"{parsed}");
         }
     }
+
+    public sealed class TimeSpanTests
+    {
+        private readonly ITestOutputHelper _output;
+
+        public TimeSpanTests(ITestOutputHelper output)
+        {
+            this._output = output ?? throw new ArgumentNullException(nameof(output));
+        }
+
+        [Theory]
+        [InlineData("07:43:07.2762671")]
+        [InlineData("12:43:07.2762671")]
+        [InlineData("22:12:43:07.2762671")]
+        [InlineData("704:12:43:07.2762671")]
+        public void Parse(string value)
+        {
+            TimeSpan ts = TimeSpan.Parse(value);
+
+            this._output.WriteLine(ts.ToString(@"dd\ \d\a\y\s\,\ hh\ \h\o\u\r\s\ mm\ \m\i\n\s\ ss\ \s\e\c\o\n\d\s"));
+        }
+    }
 }
