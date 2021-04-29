407d3a63b79f6c0ee5a95202fd60f4937c3c239f
Mark Ridgwell <credfeto@users.noreply.github.com>
2021-04-29 10:54:16 +0100

    Added timespan tests to experiment with formatting

---
+++ src/Experiments/TimeSpanTests.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Experiments
+{
+    public sealed class TimeSpanTests
+    {
+        private readonly ITestOutputHelper _output;
+
+        public TimeSpanTests(ITestOutputHelper output)
+        {
+            this._output = output ?? throw new ArgumentNullException(nameof(output));
+        }
+
+        [Theory]
+        [InlineData("07:43:07.2762671")]
+        [InlineData("12:43:07.2762671")]
+        [InlineData("22:12:43:07.2762671")]
+        [InlineData("704:12:43:07.2762671")]
+        [InlineData("00:04:19.3520847")]
+        public void Parse(string value)
+        {
+            TimeSpan ts = TimeSpan.Parse(value);
+
+            this._output.WriteLine(ts.ToString(@"dd\ \d\a\y\s\,\ hh\ \h\o\u\r\s\ mm\ \m\i\n\s\ ss\ \s\e\c\o\n\d\s"));
+        }
+    }
+}
