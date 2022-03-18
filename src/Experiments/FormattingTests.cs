e33ad14c69d1709695fbd2882a02d9fa97d40775
Mark Ridgwell <credfeto@users.noreply.github.com>
2022-03-18 18:30:28 +0000

    Test as memo

---
+++ src/Experiments/FormattingTests.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Globalization;
+using FunFair.Test.Common;
+using Xunit;
+using Xunit.Abstractions;
+
+namespace Experiments;
+
+public sealed class FormattingTests : TestBase
+{
+    private readonly ITestOutputHelper _output;
+
+    public FormattingTests(ITestOutputHelper output)
+    {
+        this._output = output ?? throw new ArgumentNullException(nameof(output));
+    }
+
+    [Theory]
+    [InlineData(1.0, "1")]
+    [InlineData(1.1, "1.1")]
+    [InlineData(1.01, "1.01")]
+    public void Format(double value, string expected)
+    {
+        string actual = value.ToString("0.##", CultureInfo.CurrentCulture);
+
+        this._output.WriteLine($"Actual: {actual}");
+
+        Assert.Equal(expected, actual);
+    }
+}
