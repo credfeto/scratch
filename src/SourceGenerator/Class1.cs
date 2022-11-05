4c185dd7e3c36d00e93f6809758860c7c9d88738
Mark Ridgwell <credfeto@users.noreply.github.com>
2022-11-05 12:38:21 +0000

    Empty Source Geneator

--- src/SourceGenerator/Class1.cs
+++
@@ -1,5 +0,0 @@
-﻿namespace SourceGenerator;
-
-public class Class1
-{
-}
---
+++ src/SourceGenerator/EnumGenerator.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.CodeAnalysis;
+
+namespace SourceGenerator;
+
+[Generator]
+public sealed class EnumGenerator : ISourceGenerator
+{
+    public void Execute(GeneratorExecutionContext context)
+    {
+        // TODO - actual source generator goes here!
+    }
+
+    public void Initialize(GeneratorInitializationContext context)
+    {
+        // No initialization required for this one
+    }
+}
