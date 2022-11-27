d59c5e37bbd34f55e55fe43115b8de75ab96c24f
Mark Ridgwell <credfeto@users.noreply.github.com>
2022-11-27 20:35:06 +0000

    Removed

--- src/Bench/Program.cs
+++ src/Bench/Program.cs
@@ -1,4 +1,4 @@
-﻿#define ENUM
+﻿#define REGED
 using System.Diagnostics;
 using BenchmarkDotNet.Running;
 
@@ -9,18 +9,11 @@ public static class Program
     private static void Main()
     {
         RunRegexBenchmarks();
-        RunEnumBenchmarks();
     }
 
     [Conditional("REGEX")]
     private static void RunRegexBenchmarks()
     {
         BenchmarkRunner.Run<RegexBench>();
-    }
-
-    [Conditional("ENUM")]
-    private static void RunEnumBenchmarks()
-    {
-        BenchmarkRunner.Run<EnumBench>();
     }
 }
