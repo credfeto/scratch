801ca2f6f1d10f99a1e5c3644706e46e167bd420
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-10-30 09:41:29 +0000

    Cleanup: InlineArrayBench.cs

--- src/Bench/InlineArrayBench.cs
+++ src/Bench/InlineArrayBench.cs
@@ -9,7 +9,6 @@ namespace Bench;
 [MemoryDiagnoser(false)]
 [SuppressMessage(category: "", checkId: "SCS0005", Justification = "Needed for predictability")]
 [SuppressMessage(category: "", checkId: "CA5394", Justification = "Needed for predictability")]
-
 public abstract class InlineArrayBench : BenchBase
 {
     private const int SIZE = 32;
